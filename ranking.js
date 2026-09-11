a1a8fd816f58c5ee9ef15e62d6a49d5384f5d2c1
shake-819 <nashakekanikama@outlook.jp>
2026-09-11 10:08:37 +0900

    Update ranking.js

--- ranking.js
+++ ranking.js
@@ -161,6 +161,7 @@ const RANKING_CATEGORIES = [
       { genre: "it", difficulty: "", label: "IT用語" },
       { genre: "dev", difficulty: "", label: "実務会話" },
       { genre: "itpass", difficulty: "", label: "Iパス用語(有料)" },
+      { genre: "kihonjoho", difficulty: "", label: "基本情報(有料)" },
     ],
   },
   {
